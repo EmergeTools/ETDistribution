51ac3cb8623a209aaade5ef8c042d7750e4a3c13
Itay Brenner <itaybrenner@hotmail.com>
2024-11-14 16:15:46 -0300

    Automatically request login if backend requires it (#8)

--- Sources/Network/URLSession+Distribute.swift
+++ Sources/Network/URLSession+Distribute.swift
@@ -10,30 +10,34 @@ import Foundation
 enum RequestError: Error {
   case badRequest(String)
   case invalidData
+  case loginRequired
   case unknownError
 }
 
 extension URLSession {
   func checkForUpdate(_ request: URLRequest, completion: @escaping (Result<DistributionUpdateCheckResponse, Error>) -> Void) {
-    self.perform(request, decode: DistributionUpdateCheckResponse.self, useCamelCase: true, completion: completion) { data in
+    self.perform(request, decode: DistributionUpdateCheckResponse.self, useCamelCase: true, completion: completion) { data, statusCode in
       let errorMessage = (
         try? JSONDecoder().decode(
           DistributionUpdateCheckErrorResponse.self,
           from: data
         ).message
       ) ?? "Unknown error"
+      if statusCode == 403 {
+        return RequestError.loginRequired
+      }
       return RequestError.badRequest(errorMessage)
     }
   }
   
   func getAuthDataWith(_ request: URLRequest, completion: @escaping (Result<AuthCodeResponse, Error>) -> Void) {
-    self.perform(request, decode: AuthCodeResponse.self, useCamelCase: false, completion: completion) { data in
+    self.perform(request, decode: AuthCodeResponse.self, useCamelCase: false, completion: completion) { _, _ in
       return RequestError.badRequest("")
     }
   }
   
   func refreshAccessToken(_ request: URLRequest, completion: @escaping (Result<AuthRefreshResponse, Error>) -> Void) {
-    self.perform(request, decode: AuthRefreshResponse.self, useCamelCase: false, completion: completion) { data in
+    self.perform(request, decode: AuthRefreshResponse.self, useCamelCase: false, completion: completion) { _, _ in
       return RequestError.badRequest("")
     }
   }
@@ -42,7 +46,7 @@ extension URLSession {
                                      decode decodable: T.Type,
                                      useCamelCase: Bool = true,
                                      completion: @escaping (Result<T, Error>) -> Void,
-                                     decodeErrorData: ((Data) -> Error)?) {
+                                     decodeErrorData: ((Data, Int) -> Error)?) {
     URLSession.shared.dataTask(with: request) { (data, response, error) in
       var result: Result<T, Error> = .failure(RequestError.unknownError)
       defer {
@@ -58,7 +62,7 @@ extension URLSession {
         return
       }
       guard (200...299).contains(httpResponse.statusCode) else {
-        let error = decodeErrorData?(data) ?? RequestError.badRequest("Unknown error")
+        let error = decodeErrorData?(data, httpResponse.statusCode) ?? RequestError.badRequest("Unknown error")
         result = .failure(error)
         return
       }
