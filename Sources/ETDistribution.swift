6c959b16fca8c073308ce045b092c4700f2349a6
Itay Brenner <itay@itaysoft.com>
2025-08-06 10:14:13 -0300

    Post UIApplication.willTerminateNotification before exiting the app (#31)
    * Post UIApplication.willTerminateNotification before exiting the app
    
    * Fix typo

--- Sources/ETDistribution.swift
+++ Sources/ETDistribution.swift
@@ -276,8 +276,14 @@ public final class ETDistribution: NSObject {
       return
     }
     UIApplication.shared.open(url) { _ in
-      // We need to exit since iOS doesn't start the install until the app exits
-      exit(0)
+      // Post notification event before closing the app
+      NotificationCenter.default.post(name: UIApplication.willTerminateNotification, object: nil)
+      
+      // Close the app after a slight delay so it has time to execute code for the notification
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+        // We need to exit since iOS doesn't start the install until the app exits
+        exit(0)
+      }
     }
   }
   
